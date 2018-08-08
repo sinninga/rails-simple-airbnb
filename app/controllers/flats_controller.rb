ad73f0b033387dd7303b08f9d50cba7869eda771
Adam Sinning <asinning25@gmail.com>
2018-08-08 15:41:56 -0300

    controller added for flats

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,61 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def edit
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    respond_to do |format|
+      if @flat.save
+        format.html { redirect_to @flat, notice: 'Flat was successfully created.' }
+        format.json { render :show, status: :created, location: @flat }
+      else
+        format.html { render :new }
+        format.json { render json: @flat.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @flat.update(flat_params)
+        format.html { redirect_to @flat, notice: 'Flat was successfully updated.' }
+        format.json { render :show, status: :ok, location: @flat }
+      else
+        format.html { render :edit }
+        format.json { render json: @flat.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    respond_to do |format|
+      format.html { redirect_to flats_url, notice: 'Flat was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_flat
+      @flat = Flat.find(params[:id])
+    end
+
+    def flat_params
+      params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    end
+end
